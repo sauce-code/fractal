096a633769ed5ee55256aee4e38d47b0649703ba
Torben <mail@sauce-code.com>
2016-01-08 18:21:06 -0500

    ADD MULTIPLICATION FUNC

--- src/Complex.h
+++ src/Complex.h
@@ -2,18 +2,18 @@
 #define COMPLEX_H_
 
 struct complex {
-	double r;
-	double i;
+	double r, i;
 	double getAbsolute() {
 		return r * r + i * i;
 	}
 	complex pow2() {
-		return { r * r - i * i, 2 * r * i};
-//		r = r * r - i * i;
-//		i = 2 * r * i;
+		return {r * r - i * i, 2 * r * i};
 	}
 	inline complex operator+(complex a) {
-		return { r + a.r, i + a.i };
+		return {r + a.r, i + a.i};
+	}
+	inline complex operator*(complex a) {
+		return {r * a.r - i * a.i, r * a.i + i * a.r};
 	}
 };
 
