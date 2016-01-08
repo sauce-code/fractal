ba1d1405de25f0065f98420f3e1b62b2d1556c97
Maike Rees <maikefer_@gmx.de>
2016-01-08 14:55:15 -0500

    green.c test

--- src/fractal.cpp
+++ src/fractal.cpp
@@ -1,15 +1,19 @@
-//============================================================================
-// Name        : fractal.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+#include <GL/freeglut.h>
 
-#include <iostream>
-using namespace std;
+void green(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitWindowSize(400, 400);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutCreateWindow("green");
+
+	glutDisplayFunc(green);
+	glClearColor(0.0, 1.0, 0.0, 1.0);
+
+	glutMainLoop();
+	return (0);
 }
